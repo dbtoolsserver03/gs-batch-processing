ae59c25305dd4437883dd3e87c2bb70b926b0406
macbookm3 <dbtoolsserver03@gmail.com>
2024-12-29 11:11:49 +0900

    2

--- complete/src/main/resources/schema-all.sql
+++ complete/src/main/resources/schema-all.sql
@@ -1,7 +1,19 @@
-DROP TABLE people IF EXISTS;
+DROP DATABASE
+IF
+	EXISTS `springBatch`;
+	
+CREATE DATABASE
+IF
+	NOT EXISTS `springBatch` CHARACTER 
+	SET utf8mb4;
+	
+USE `springBatch`;
 
-CREATE TABLE people  (
-    person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20)
-);
+DROP TABLE IF EXISTS `people`;
+
+CREATE TABLE `people` (
+  `person_id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `first_name` varchar(255) DEFAULT NULL COMMENT 'first_name',
+  `last_name` varchar(255) DEFAULT NULL COMMENT 'last_name',
+  PRIMARY KEY (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='people';
